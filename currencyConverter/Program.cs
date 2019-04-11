2a228703d00c51fca6c127d53a2adaf0cb5c5618
AlexanderJarov <49072663+AlexanderJarov@users.noreply.github.com>
2019-04-11 17:26:08 +0300

    Add files via upload

---
+++ currencyConverter/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            string firstCurrency = Console.ReadLine();
+            string secondCurrency = Console.ReadLine();
+
+            double BGN = 1;
+            double USD = 1.79549;
+            double EUR = 1.95583;
+            double GBP = 2.53405;
+
+            double totalSum = 0;
+
+            if (firstCurrency == "BGN")
+            {
+                if (secondCurrency == "USD")
+                {
+                    totalSum = money / USD;
+                }
+                else if (secondCurrency == "GBP")
+                {
+                    totalSum = money / GBP;
+                }
+                else if (secondCurrency == "EUR") 
+                {
+                    totalSum = money / EUR;
+                }
+            }                        
+            if (firstCurrency == "USD")
+            {
+                if (secondCurrency == "BGN")
+                {
+                    totalSum = money * USD;
+                }
+                else if (secondCurrency == "EUR")
+                {
+                    totalSum = money * USD / EUR; 
+                }
+                else if (secondCurrency == "GBP")
+                {
+                    totalSum = money * USD / GBP;
+                }
+            }
+            if (firstCurrency == "EUR")
+            {
+                if (secondCurrency == "BGN")
+                {
+                    totalSum = money * EUR;
+                }
+                else if  (secondCurrency == "USD")
+                {
+                    totalSum = money * EUR / USD;
+                }
+                else if (secondCurrency == "GBP")
+                {
+                    totalSum = money * EUR / GBP;
+                }
+            }
+            if (firstCurrency == "GBP")
+            {
+                if (secondCurrency == "BGN")
+                {
+                    totalSum = money * GBP;
+                }
+                else if (secondCurrency == "USD")
+                {
+                    totalSum = money * GBP / USD;
+                }
+                else if (secondCurrency == "EUR")
+                {
+                    totalSum = money * GBP / EUR;
+                }
+            }
+
+            Console.WriteLine($"{totalSum:f2} {secondCurrency}");
+
+        }
+    }
+}
